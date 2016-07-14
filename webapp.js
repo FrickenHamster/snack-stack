444615642247db8bf5d7576f730aad4bc7fab375
Alex Yan <alex.yan@appfolio.com>
2016-07-14 10:57:59 -0700

    add index page

--- webapp.js
+++ webapp.js
@@ -9,3 +9,18 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+
+
+var server = app.listen(3154, function()
+{
+	let port = server.address().port;
+	serverLog("Server started on port " + port);
+});
+
+
+function serverLog(str) 
+{
+	var date = new Date();
+	var dtext = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	console.log(dtext + "-" + str);
+}
